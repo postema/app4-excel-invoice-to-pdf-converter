37e87dab18aa69e2c301004cba4ca1e81a8d30ff
postema <pj.postema@gmail.com>
2023-03-22 13:54:50 +0100

    header complete with nr and date

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ from pathlib import Path
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
+    # prepare the dataframe
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # prepare the pdf
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf")
 
+    # prepare data for header
+    filename = Path(filepath).stem
+    invoice_nr, i_date = filename.split("-")
+
+    # pdf header text
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {i_date}", ln=1)
+
+
+    # pdf body text = table
+
+    # pdf footer text
+
+    pdf.output(f"PDFs/{filename}.pdf")
